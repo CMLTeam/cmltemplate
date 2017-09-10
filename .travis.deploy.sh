9d213c09249e488672ea5135cd07dd19042049a7
vgubarkov <xonixx@gmail.com>
2017-09-10 22:45:32 +0300

    travis ci

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -10,7 +10,7 @@ scp -i /tmp/deploy_rsa $APP.conf target/$APP.jar $USER@$SERV:~/
 ssh -i /tmp/deploy_rsa $USER@$SERV "
 if [ ! -f /etc/init.d/$APP ]
 then
-    sudo ln -s /home/apps/$APP.jar /etc/init.d/$APP
+    sudo ln -s /home/$USER/$APP.jar /etc/init.d/$APP
 fi
 sudo /etc/init.d/$APP restart
 "
