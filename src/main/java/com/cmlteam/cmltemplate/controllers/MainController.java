c519ea32aa858adf72a3187eb0a23fd0e9d0dd0e
vgubarkov <xonixx@gmail.com>
2018-01-31 14:27:07 +0200

    db

--- src/main/java/com/cmlteam/cmltemplate/controllers/MainController.java
+++ src/main/java/com/cmlteam/cmltemplate/controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.cmlteam.cmltemplate.controllers;
 
 import com.cmlteam.cmltemplate.model.ServerStatus;
+import com.cmlteam.cmltemplate.services.SampleService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,16 +15,21 @@ public class MainController {
 
     private static final Logger log = LoggerFactory.getLogger(MainController.class);
 
+    private final SampleService sampleService;
+
     @Autowired
-    public MainController() {
+    public MainController(SampleService sampleService) {
+        this.sampleService = sampleService;
     }
 
-    @RequestMapping(
-            value = "/webhook",
-            method = RequestMethod.GET)
-    public String getWebHook() {
+    @RequestMapping(value = "/testws", method = RequestMethod.GET)
+    public String testws() {
         RestTemplate template = new RestTemplate();
         ServerStatus status = template.getForObject("https://l2c1x1.com/services/misc/server-stats", ServerStatus.class);
         return "" + status.totalAccounts;
+    }
+    @RequestMapping(value = "/testdb", method = RequestMethod.GET)
+    public String testdb() {
+        return sampleService.getDbVersion();
     }
 }
