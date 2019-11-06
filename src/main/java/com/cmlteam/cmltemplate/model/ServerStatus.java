7d305d25fcc3448954661d93163a9c1ec056ba05
NineKFlames <cheberiak.av@gmail.com>
2019-11-06 14:52:14 +0200

    Fix server status vulnerability

--- src/main/java/com/cmlteam/cmltemplate/model/ServerStatus.java
+++ src/main/java/com/cmlteam/cmltemplate/model/ServerStatus.java
@@ -1,8 +1,16 @@
 package com.cmlteam.cmltemplate.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ServerStatus {
-  public int totalAccounts;
+  private int totalAccounts;
 }
