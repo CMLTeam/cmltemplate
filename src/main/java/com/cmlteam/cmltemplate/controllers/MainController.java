2e203d455e83781e32e6bce75ae78d1eb5bcb311
xonix <xonixx@gmail.com>
2020-03-31 01:40:48 +0300

    update mvn deps

--- src/main/java/com/cmlteam/cmltemplate/controllers/MainController.java
+++ src/main/java/com/cmlteam/cmltemplate/controllers/MainController.java
@@ -3,6 +3,7 @@ package com.cmlteam.cmltemplate.controllers;
 import com.cmlteam.cmltemplate.model.ServerStatus;
 import com.cmlteam.cmltemplate.services.SampleService;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,24 +16,20 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-@RestController
 @Slf4j
+@RequiredArgsConstructor
+@RestController
 public class MainController {
 
   private final SampleService sampleService;
 
-  @Autowired
-  public MainController(SampleService sampleService) {
-    this.sampleService = sampleService;
-  }
-
   @ApiOperation(value = "Test GET endpoint")
-  @GetMapping
+  @GetMapping(value = "test")
   public String test() {
     return "Hello CML!";
   }
 
-  @GetMapping("testws")
+  @GetMapping(value = "testws")
   public String testws() {
     RestTemplate template = new RestTemplate();
     ServerStatus status =
@@ -43,13 +40,13 @@ public class MainController {
   }
 
   @ApiOperation(value = "Show the DB version")
-  @GetMapping("testdb")
+  @GetMapping(value = "testdb")
   public String testdb() {
     return sampleService.getDbVersion();
   }
 
   @ApiOperation(value = "Test POST endpoint")
-  @PostMapping(value = "/testpost")
+  @PostMapping(value = "/testpost", produces = "application/json;charset=UTF-8")
   public Map testPost(@RequestBody Map payload) {
     log.info("Test POST: {}", payload);
     HashMap res = new HashMap();
