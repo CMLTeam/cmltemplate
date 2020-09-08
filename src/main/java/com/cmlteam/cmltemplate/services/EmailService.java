f0a196a95bc9704f5ea8745219be3870d387e0f8
xonix <xonixx@gmail.com>
2020-09-08 03:04:29 +0300

    Add EmailService #19 : attachment

--- src/main/java/com/cmlteam/cmltemplate/services/EmailService.java
+++ src/main/java/com/cmlteam/cmltemplate/services/EmailService.java
@@ -3,13 +3,12 @@ package com.cmlteam.cmltemplate.services;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @Service
 @RequiredArgsConstructor
@@ -19,31 +18,43 @@ public class EmailService {
   private final JavaMailSender javaMailSender;
 
   @SneakyThrows
-  public void sendEmail(String from, String to, String subject, String body, boolean html) {
-    if (html) {
-      MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-      mimeMessage.setSubject(subject);
-      MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-      helper.setFrom(from);
-      helper.setTo(to);
-      helper.setText(body, true);
-      javaMailSender.send(mimeMessage);
-    } else {
-      SimpleMailMessage message = new SimpleMailMessage();
-      message.setFrom(from);
-      message.setTo(to);
-      message.setSubject(subject);
-      message.setText(body);
-      javaMailSender.send(message);
+  public void sendEmail(
+      String from, String to, String subject, String body, boolean html, File attachment) {
+
+    MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+    mimeMessage.setSubject(subject);
+
+    MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+    helper.setFrom(from);
+    helper.setTo(to);
+    helper.setText(body, html);
+
+    if (attachment != null) {
+      helper.addAttachment(attachment.getName(), attachment);
     }
+
+    javaMailSender.send(mimeMessage);
   }
 
-  @PostConstruct
+/*  @PostConstruct
   public void test() {
     log.info("Send test email...");
     String d = "cmlteam.com";
+//    sendEmail(
+//        "no-reply@" + d,
+//        "volodymyr@" + d,
+//        "subj",
+//        "body 111 <h1>Header</h1> <b>Bold</b>",
+//        true,
+//        new File("/home/xonix/Desktop/test2.jpg"));
     sendEmail(
-        "no-reply@" + d, "volodymyr@" + d, "subj", "body 111 <h1>Header</h1> <b>Bold</b>", true);
+        "no-reply@" + d,
+        "volodymyr@" + d,
+        "subj",
+        "body 111 <h1>Header</h1> <b>Bold</b>",
+        false,
+        null);
     log.info("Sent.");
-  }
+  }*/
 }
