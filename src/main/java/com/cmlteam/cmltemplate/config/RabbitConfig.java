4483cf408d2584992be5b4da566fc3eb64eaa47b
xonix <xonixx@gmail.com>
2021-02-26 22:38:42 +0200

    Pre-configure RabbitMQ #43 - impr

--- src/main/java/com/cmlteam/cmltemplate/config/RabbitConfig.java
+++ src/main/java/com/cmlteam/cmltemplate/config/RabbitConfig.java
@@ -1,11 +1,9 @@
 package com.cmlteam.cmltemplate.config;
 
-import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -33,17 +31,5 @@ public class RabbitConfig {
   @Bean
   Binding binding(Queue queue, DirectExchange exchange) {
     return BindingBuilder.bind(queue).to(exchange).with(routingKey);
-  }
-
-  @Bean
-  public MessageConverter jsonMessageConverter() {
-    return new Jackson2JsonMessageConverter();
-  }
-
-  @Bean
-  public AmqpTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-    final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-    rabbitTemplate.setMessageConverter(jsonMessageConverter());
-    return rabbitTemplate;
   }
 }
