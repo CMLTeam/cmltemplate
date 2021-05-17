3a2260a89d48c2812e412040443dc0ea7d3fc5bf
Ihor Tarkhan <igor.tark@gmail.com>
2021-05-17 19:39:59 +0300

    util and service moved

---
+++ core/src/main/java/com/cmlteam/cmltemplate/services/SampleService.java
@@ -0,0 +1,24 @@
+package com.cmlteam.cmltemplate.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+  private final JdbcTemplate jdbcTemplate;
+
+  @Autowired
+  public SampleService(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  public String getDbVersion() {
+    return jdbcTemplate.queryForObject("select version();", String.class);
+  }
+
+//  @PostConstruct
+//  public void test() {
+//    System.out.println(getDbVersion());
+//  }
+}
