92fd2f80eacefc48e38b463ecbceda9a7026e3f0
xonix <xonixx@gmail.com>
2021-11-06 16:42:20 +0200

    Pre-configure Mongo DB #23

--- src/main/java/com/cmlteam/cmltemplate/services/SampleService.java
+++ src/main/java/com/cmlteam/cmltemplate/services/SampleService.java
@@ -1,6 +1,8 @@
 package com.cmlteam.cmltemplate.services;
 
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
@@ -9,18 +11,24 @@ import javax.annotation.PostConstruct;
 @Service
 public class SampleService {
   private final JdbcTemplate jdbcTemplate;
+  private final MongoTemplate mongoTemplate;
 
   @Autowired
-  public SampleService(JdbcTemplate jdbcTemplate) {
+  public SampleService(JdbcTemplate jdbcTemplate, MongoTemplate mongoTemplate) {
     this.jdbcTemplate = jdbcTemplate;
+    this.mongoTemplate = mongoTemplate;
   }
 
   public String getDbVersion() {
     return jdbcTemplate.queryForObject("select version();", String.class);
   }
+  public String getMongoVersion() {
+    return mongoTemplate.executeCommand(new Document("buildInfo",1)).get("version", String.class);
+  }
 
-//  @PostConstruct
-//  public void test() {
+  @PostConstruct
+  public void test() {
 //    System.out.println(getDbVersion());
-//  }
+//    System.out.println("MONGO: " + getMongoVersion());
+  }
 }
