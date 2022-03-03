50e18c622b317caa5ec928b26468ed25ec759d59
xonix <xonixx@gmail.com>
2022-03-03 21:01:59 +0200

    add ability to set $PROJ_PATH

--- init.sh
+++ init.sh
@@ -28,10 +28,14 @@ fail() {
     exit 1
 }
 
-proj_path="$(pwd)/$proj_name"
-if [[ -e "$proj_path" ]]
-then
-    fail "Path $proj_path exists already."
+if [[ -z $PROJ_PATH ]]
+  proj_path="$(pwd)/$proj_name"
+  if [[ -e "$proj_path" ]]
+  then
+      fail "Path $proj_path exists already."
+  fi
+else
+  proj_path="$(cd "$PROJ_PATH"; pwd)"
 fi
 
 download() {
