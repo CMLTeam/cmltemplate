1ea86ae12d6b69a46db5aae3aa7da7ae0335bff4
xonix <xonixx@gmail.com>
2023-04-05 00:29:54 +0300

    Add basic functionality for encryption / decryption #38

---
+++ src/test/java/com/cmlteam/cmltemplate/util/AesHelperTests.java
@@ -0,0 +1,22 @@
+package com.cmlteam.cmltemplate.util;
+
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AesHelperTests {
+  @Test
+  void test1() {
+    // GIVEN
+    AesHelper aesHelper = new AesHelper("key-must-be-16-b".getBytes(StandardCharsets.UTF_8));
+    String input = "hello world 123";
+    byte[] inputBytes = input.getBytes(StandardCharsets.UTF_8);
+
+    // WHEN
+    byte[] encrypted = aesHelper.encrypt(inputBytes);
+    byte[] result = aesHelper.decrypt(encrypted);
+
+    // THEN
+    Assertions.assertEquals(inputBytes, result);
+  }
+}
