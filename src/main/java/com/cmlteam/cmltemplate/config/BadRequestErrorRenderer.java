c29f3ef11d1b3e9b2a9993692d3d1318c365a5cf
xonix <xonixx@gmail.com>
2023-04-04 14:00:24 +0300

    Provide default `@ControllerAdvice` #75

---
+++ src/main/java/com/cmlteam/cmltemplate/config/BadRequestErrorRenderer.java
@@ -0,0 +1,30 @@
+package com.cmlteam.cmltemplate.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+@Slf4j
+public class BadRequestErrorRenderer {
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public @ResponseBody Map<String, String> handleValidationExceptions(
+      MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        log.warn("Was error: " + ex);
+        return errors;
+    }
+}
